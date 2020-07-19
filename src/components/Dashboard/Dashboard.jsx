1136acf7bcfa2142bc580d55b4b09e80bdd7b827
Ahmed Dambatta Hassan <hassanamir643@yahoo.com>
2020-07-19 01:44:30 +0100

    dashboard and edit profile done

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,7 +1,178 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import {
+  Typography,
+  Card,
+  CardContent,
+  Table,
+  Grid,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  CircularProgress,
+  Chip,
+} from "@material-ui/core";
+import axios from "axios";
+import moment from "moment";
+import { Alert, AlertTitle } from "@material-ui/lab";
+import EditProfile from "./EditProfile";
 
 const Dashboard = () => {
-  return <div className="content"></div>;
+  const token = localStorage.getItem("token");
+  const [store, setStore] = useState({});
+
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    fetchStore();
+  }, []);
+
+  const fetchStore = () => {
+    setLoading(true);
+    axios
+      .get(
+        `https://jenifa-stores.herokuapp.com/stores
+      `,
+        {
+          headers: { Authorization: `${token}` },
+        }
+      )
+      .then((res) => {
+        console.log(res.data);
+        setStore(res.data.store);
+
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error.response);
+        setLoading(false);
+      });
+  };
+
+  return (
+    <div className="content">
+      {loading ? (
+        <CircularProgress style={{ marginLeft: "50%" }} />
+      ) : (
+        <>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <Card elevation={0}>
+                <CardContent>
+                  <Typography> </Typography>
+                  <Typography variant="h4"></Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Card elevation={0}>
+                <CardContent>
+                  <Typography> </Typography>
+                  <Typography variant="h4"></Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Card elevation={0}>
+                <CardContent>
+                  <Typography></Typography>
+                  <Typography variant="h4"></Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <Card elevation={0}>
+                <CardContent>
+                  <Typography>Joined</Typography>
+                  <Typography variant="h4">
+                    {" "}
+                    {moment(store.createdA).format("DD MMM, YYYY")}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={12} sm={12}>
+              <Card elevation={0}>
+                <CardContent>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                      <TableContainer component={Paper} elevation={0}>
+                        <Table aria-label="customized table">
+                          <TableHead>
+                            <TableRow>
+                              <TableCell style={{ fontWeight: 400 }}>
+                                Store Name
+                              </TableCell>
+                              <TableCell>{store.storeName}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                              <TableCell style={{ fontWeight: 400 }}>
+                                Email Address
+                              </TableCell>
+                              <TableCell>{store.email}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                              <TableCell style={{ fontWeight: 400 }}>
+                                Location
+                              </TableCell>
+                              <TableCell>{store.location}</TableCell>
+                            </TableRow>
+                          </TableHead>
+                        </Table>
+                      </TableContainer>
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TableContainer component={Paper} elevation={0}>
+                        <Table aria-label="customized table">
+                          <TableHead>
+                            <TableRow>
+                              <TableCell style={{ fontWeight: 400 }}>
+                                State
+                              </TableCell>
+                              <TableCell>{store.state}</TableCell>
+                            </TableRow>
+
+                            <TableRow>
+                              <TableCell style={{ fontWeight: 400 }}>
+                                Phone Number
+                              </TableCell>
+                              <TableCell>{store.phoneNumber}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                              <TableCell style={{ fontWeight: 400 }}>
+                                Phone Number 2
+                              </TableCell>
+                              <TableCell>{store.phone_number_two}</TableCell>
+                            </TableRow>
+                            <TableRow>
+                              <TableCell style={{ fontWeight: 400 }}>
+                                Phone Number 3
+                              </TableCell>
+                              <TableCell>{store.phone_number_three}</TableCell>
+                            </TableRow>
+                          </TableHead>
+                        </Table>
+                      </TableContainer>
+                    </Grid>
+                    <Grid item xs={12} sm={12}>
+                      <Card elevation={0}>
+                        <CardContent>
+                          <Typography>Other Actions</Typography>
+                          <hr />
+                          <div style={{ marginBottom: "20px" }}></div>
+                          <EditProfile store={store} />
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  </Grid>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Dashboard;
---
+++ src/components/Dashboard/EditProfile.jsx
@@ -0,0 +1,383 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  useMediaQuery,
+  useTheme,
+  makeStyles,
+  LinearProgress,
+  Snackbar,
+  Slide,
+  Grid,
+  TextField,
+  Tooltip,
+} from "@material-ui/core";
+import axios from "axios";
+import { Formik } from "formik";
+import * as Yup from "yup";
+import { Alert, AlertTitle } from "@material-ui/lab";
+import { Create } from "@material-ui/icons";
+
+const useStyles = makeStyles((theme) => ({
+  dialog: {
+    width: "100%",
+  },
+  error: {
+    color: "rgb(235, 54, 54)",
+    marginTop: "-20px",
+    marginBottom: "10px",
+  },
+}));
+
+const EditProfile = ({ store }) => {
+  console.log(store);
+  const [open, setOpen] = useState(false);
+  const [al, setAl] = useState(false);
+  const [err, setErr] = useState(false);
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [severity, setSeverity] = useState("success");
+  const theme = useTheme();
+  const token = localStorage.getItem("token");
+
+  const {
+    storeName,
+    location,
+    address,
+    email,
+    state,
+    phoneNumber,
+    phone_number_two,
+    phone_number_three,
+  } = store;
+
+  const fullScreen = useMediaQuery(theme.breakpoints.down("sm"));
+  const classes = useStyles();
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  return (
+    <Formik
+      enableReinitialize={true}
+      initialValues={{
+        storeName,
+        location,
+        address,
+        email,
+        state,
+        phoneNumber,
+        phone_number_two,
+        phone_number_three,
+      }}
+      onSubmit={(values, { setSubmitting, resetForm }) => {
+        setTimeout(() => {
+          console.log("Editing profile", values);
+          setLoading(true);
+          axios
+            .patch(
+              `https://jenifa-stores.herokuapp.com/stores/edit
+          `,
+              values,
+              { headers: { Authorization: `${token}` } }
+            )
+            .then((res) => {
+              console.log(res);
+              console.log(res.data);
+              setMessage(res.data.message);
+              setAl(true);
+              setLoading(false);
+              resetForm({});
+              setTimeout(() => {
+                window.location.reload(false);
+              }, 1000);
+            })
+            .catch((err) => {
+              console.log(err.response);
+              setMessage("Profile could not be updated, try again!");
+              setErr(true);
+              setAl(true);
+              setSeverity("error");
+              setLoading(false);
+            });
+          setSubmitting(false);
+        }, 200);
+      }}
+      validationSchema={Yup.object().shape({
+        storeName: Yup.string().min(2, "The first name can not be less than 2"),
+
+        location: Yup.string(),
+
+        address: Yup.string().min(2, "The address can not be less than 2"),
+
+        email: Yup.string().email("Invalid email"),
+        state: Yup.string(),
+        phoneNumber: Yup.string(),
+        phone_number_two: Yup.string(),
+        phone_number_three: Yup.string(),
+      })}
+    >
+      {(props) => {
+        const {
+          values,
+          touched,
+          errors,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        } = props;
+        return (
+          <div>
+            <Tooltip title="Click to edit profile" arrow>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={handleClickOpen}
+              >
+                <Create style={{ marginRight: "5px" }} /> Edit Profile
+              </Button>
+            </Tooltip>
+            <Dialog
+              fullScreen={fullScreen}
+              open={open}
+              maxWidth="md"
+              onClose={handleClose}
+              fullWidth
+              aria-labelledby="responsive-dialog-title"
+            >
+              <DialogTitle id="responsive-dialog-title">
+                {"Edit Profile"}
+              </DialogTitle>
+              {al ? (
+                <>
+                  <Alert severity={severity}>
+                    <AlertTitle>{severity}</AlertTitle>
+                    {message}
+                  </Alert>
+                  <Snackbar
+                    open={open}
+                    // autoHideDuration={3000}
+                    TransitionComponent={Slide}
+                    onClose={handleClose}
+                  >
+                    <Alert severity={severity}>{message}</Alert>
+                  </Snackbar>
+                </>
+              ) : (
+                <div></div>
+              )}
+              <form onSubmit={handleSubmit}>
+                <DialogContent>
+                  <Grid item xs={12}>
+                    <div>
+                      <TextField
+                        name="storeName"
+                        label="Store Name *"
+                        fullWidth
+                        variant="outlined"
+                        type="text"
+                        error={err}
+                        value={values.storeName}
+                        style={{ marginBottom: "20px" }}
+                        className={
+                          errors.storeName && touched.storeName && "error"
+                        }
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                      {errors.storeName && touched.storeName && (
+                        <div className={classes.error}>
+                          {" "}
+                          {errors.storeName}{" "}
+                        </div>
+                      )}
+                    </div>
+                    <div>
+                      <TextField
+                        name="location"
+                        label="Location *"
+                        fullWidth
+                        variant="outlined"
+                        type="text"
+                        error={err}
+                        value={values.location}
+                        style={{ marginBottom: "20px" }}
+                        className={
+                          errors.location && touched.location && "error"
+                        }
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                      />
+                      {errors.location && touched.location && (
+                        <div className={classes.error}> {errors.location} </div>
+                      )}
+                    </div>
+                    <div>
+                      <TextField
+                        name="address"
+                        label="Address"
+                        multiline
+                        variant="outlined"
+                        fullWidth
+                        type="text"
+                        error={err}
+                        value={values.address || ""}
+                        className={errors.address && touched.address && "error"}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        style={{ marginBottom: "20px", marginRight: "10px" }}
+                      />
+
+                      {errors.address && touched.address && (
+                        <div className={classes.error}> {errors.address} </div>
+                      )}
+                    </div>
+                    <div>
+                      <TextField
+                        name="email"
+                        label="Email Address"
+                        fullWidth
+                        variant="outlined"
+                        type="email"
+                        error={err}
+                        value={values.email || ""}
+                        className={errors.email && touched.email && "error"}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        style={{ marginBottom: "20px" }}
+                      />
+                      {errors.email && touched.email && (
+                        <div className={classes.error}> {errors.email} </div>
+                      )}
+                    </div>
+                    <div>
+                      <TextField
+                        name="state"
+                        label="State"
+                        variant="outlined"
+                        fullWidth
+                        type="text"
+                        error={err}
+                        value={values.state || ""}
+                        className={errors.state && touched.state && "error"}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        style={{ marginBottom: "20px" }}
+                      />
+                      {errors.state && touched.state && (
+                        <div className={classes.error}> {errors.state} </div>
+                      )}
+                    </div>
+
+                    <div>
+                      <TextField
+                        name="phoneNumber"
+                        label="Phone Number 1"
+                        fullWidth
+                        variant="outlined"
+                        type="text"
+                        error={err}
+                        value={values.phoneNumber || ""}
+                        className={
+                          errors.phoneNumber && touched.phoneNumber && "error"
+                        }
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        style={{ marginBottom: "20px" }}
+                      />
+                      {errors.phoneNumber && touched.phoneNumber && (
+                        <div className={classes.error}>
+                          {" "}
+                          {errors.phoneNumber}{" "}
+                        </div>
+                      )}
+                    </div>
+
+                    <div>
+                      <TextField
+                        name="phone_number_two"
+                        label="Phone Number 2 "
+                        fullWidth
+                        variant="outlined"
+                        type="text"
+                        error={err}
+                        value={values.phone_number_two || ""}
+                        className={
+                          errors.phone_number_two &&
+                          touched.phone_number_two &&
+                          "error"
+                        }
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        style={{ marginBottom: "20px" }}
+                      />
+                      {errors.phone_number_two && touched.phone_number_two && (
+                        <div className={classes.error}>
+                          {" "}
+                          {errors.phone_number_two}{" "}
+                        </div>
+                      )}
+                    </div>
+                    <div>
+                      <TextField
+                        name="phone_number_three"
+                        label="Phone Number 3"
+                        fullWidth
+                        type="text"
+                        variant="outlined"
+                        error={err}
+                        value={values.phone_number_three || ""}
+                        className={
+                          errors.phone_number_three &&
+                          touched.phone_number_three &&
+                          "error"
+                        }
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        style={{ marginBottom: "20px" }}
+                      />
+                      {errors.phone_number_three &&
+                        touched.phone_number_three && (
+                          <div className={classes.error}>
+                            {errors.phone_number_three}
+                          </div>
+                        )}
+                    </div>
+                  </Grid>
+                </DialogContent>
+                <DialogActions>
+                  <Button autoFocus onClick={handleClose} color="secondary">
+                    Cancel
+                  </Button>
+                  <Button
+                    onClick={handleSubmit}
+                    disabled={loading}
+                    color="primary"
+                    autoFocus
+                  >
+                    Submit
+                  </Button>
+                  {loading && (
+                    <LinearProgress
+                      variant="query"
+                      style={{ marginTop: "10px" }}
+                    />
+                  )}
+                </DialogActions>
+              </form>
+            </Dialog>
+          </div>
+        );
+      }}
+    </Formik>
+  );
+};
+
+export default EditProfile;
