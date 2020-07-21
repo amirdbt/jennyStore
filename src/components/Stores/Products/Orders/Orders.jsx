368c2023ff74ee4430e01265afba23048a03d9d8
Ahmed Dambatta Hassan <hassanamir643@yahoo.com>
2020-07-21 15:10:53 +0100

    Added confirm, reject and search order bookings features

--- src/components/Stores/Products/Orders/Orders.jsx
+++ src/components/Stores/Products/Orders/Orders.jsx
@@ -17,27 +17,21 @@ import {
   Grid,
   Card,
   Chip,
-  TextField,
-  MenuItem,
   Typography,
+  Snackbar,
+  Slide,
   Button,
 } from "@material-ui/core";
 import {
-  AccountCircle,
-  ArrowForward,
   FirstPage,
   KeyboardArrowLeft,
   KeyboardArrowRight,
   LastPage,
-  Image,
-  Edit,
-  AddCircle,
 } from "@material-ui/icons";
-import { Link } from "react-router-dom";
 import moment from "moment";
 import axios from "axios";
-// import SearchBox from "../../Utility/SearchBox";
-import EditOrder from "./EditOrder";
+import { Alert, AlertTitle } from "@material-ui/lab";
+import SearchBox from "../../../Utility/SearchBox";
 
 const useStyles = makeStyles((theme) => ({
   head: {
@@ -127,8 +121,15 @@ const Orders = () => {
   const [loading, setLoading] = useState(false);
   const token = localStorage.getItem("token");
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const [al, setAl] = useState(false);
+  const [err, setErr] = useState(false);
+  const [message, setMessage] = useState("");
+  const [severity, setSeverity] = useState("success");
 
   const [searchField, setSearchField] = useState("");
 
@@ -138,7 +139,9 @@ const Orders = () => {
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
-
+  const handleClose = () => {
+    setOpen(false);
+  };
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
@@ -158,7 +161,7 @@ const Orders = () => {
       })
       .then((res) => {
         console.log(res);
-        setOrders(res.data.bookings);
+        setOrders(res.data.userBookings);
         setLoading(false);
       })
       .catch((error) => {
@@ -167,18 +170,122 @@ const Orders = () => {
       });
   };
   const filteredOrders = orders.filter((order) => {
-    return order.lastName.toLowerCase().includes(searchField.toLowerCase());
+    return order.booking_status
+      .toLowerCase()
+      .includes(searchField.toLowerCase());
   });
+
+  const checkStatus = (status) => {
+    if (status === "pending") {
+      return (
+        <Chip variant="default" label="PENDING" style={{ color: "#ff9800" }} />
+      );
+    } else if (status === "confirmed") {
+      return (
+        <Chip
+          variant="default"
+          label="CONFIRMED"
+          style={{ color: "#4caf50" }}
+        />
+      );
+    } else {
+      return (
+        <Chip variant="default" label="REJECTED" style={{ color: "#e53935" }} />
+      );
+    }
+  };
+
+  const confirm = (booking_id) => {
+    axios
+      .put(
+        `https://jenifa-stores.herokuapp.com/bookings/confirm/${booking_id}`,
+        "",
+        {
+          headers: { Authorization: `${token}` },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        setMessage("Booking confirmed successfully");
+        setAl(true);
+        setTimeout(() => {
+          window.location.reload(false);
+        }, 1000);
+      })
+      .catch((err) => {
+        console.log(err);
+        setMessage("Booking could not be confirmed, try again");
+        setErr(true);
+        setAl(true);
+        setSeverity("error");
+        setTimeout(() => {
+          window.location.reload(false);
+        }, 1000);
+      });
+  };
+  const reject = (booking_id) => {
+    axios
+      .patch(
+        `https://jenifa-stores.herokuapp.com/bookings/reject/${booking_id}`,
+        "",
+        {
+          headers: { Authorization: `${token}` },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+        setMessage("Booking rejected successfully");
+        setAl(true);
+        setTimeout(() => {
+          window.location.reload(false);
+        }, 1000);
+      })
+      .catch((err) => {
+        console.log(err);
+        setMessage("Booking could not be rejected, try again");
+        setErr(true);
+        setAl(true);
+        setSeverity("error");
+        setTimeout(() => {
+          window.location.reload(false);
+        }, 1000);
+      });
+  };
+
   return (
     <div className="content">
       {loading ? (
         <CircularProgress style={{ marginLeft: "50%" }} />
       ) : (
         <>
+          {al ? (
+            <>
+              <Alert severity={severity}>
+                <AlertTitle>{severity}</AlertTitle>
+                {message}
+              </Alert>
+              <Snackbar
+                open={open}
+                // autoHideDuration={3000}
+                TransitionComponent={Slide}
+                onClose={handleClose}
+              >
+                <Alert severity={severity}>{message}</Alert>
+              </Snackbar>
+            </>
+          ) : (
+            <div></div>
+          )}
           <div style={{ marginBottom: "10px" }}></div>
           <Grid container spacing={2}>
-            <Grid item xs={6}>
+            <Grid item xs={12} sm={6}>
               <Typography variant="h4">All Orders</Typography>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <SearchBox
+                place="Search Bookings by status..."
+                searchChange={searchChange}
+              />
             </Grid>
           </Grid>
           <div style={{ marginBottom: "20px" }}></div>
@@ -193,6 +300,10 @@ const Orders = () => {
                         <TableCell className={classes.head}>Customer</TableCell>
                         <TableCell className={classes.head}>Category</TableCell>
                         <TableCell className={classes.head}>Status</TableCell>
+                        <TableCell className={classes.head}>Message</TableCell>
+                        <TableCell className={classes.head}>
+                          Product Name
+                        </TableCell>
                         <TableCell className={classes.head}>
                           Order Date
                         </TableCell>
@@ -206,19 +317,52 @@ const Orders = () => {
                             page * rowsPerPage + rowsPerPage
                           )
                         : filteredOrders
-                      ).map((order) => (
-                        <TableRow key={order.id}>
-                          <TableCell>{order.firstName}</TableCell>
-                          <TableCell>{order.userName}</TableCell>
-                          <TableCell>{order.category}</TableCell>
-                          <TableCell>{order.message}</TableCell>
+                      ).map((order, index) => (
+                        <TableRow key={index}>
+                          <TableCell>{index}</TableCell>
+                          <TableCell>{order.customer_user_name}</TableCell>
+                          <TableCell>{order.product_category}</TableCell>
+
+                          <TableCell>
+                            {checkStatus(order.booking_status)}
+                          </TableCell>
+                          <TableCell>{order.booking_message}</TableCell>
+                          <TableCell>{order.product_name}</TableCell>
                           <TableCell>
                             {" "}
                             {moment(order.createdA).format("DD MMM, YYYY")}
                           </TableCell>
-                          <TableCell>
-                            {" "}
-                            <EditOrder />
+                          <TableCell style={{ display: "flex" }}>
+                            <Button
+                              variant="outlined"
+                              size="small"
+                              style={{ color: "#4caf50", marginRight: 5 }}
+                              onClick={() => {
+                                if (
+                                  window.confirm(
+                                    "Are you sure you want to confirm this order?"
+                                  )
+                                )
+                                  confirm(order.booking_id);
+                              }}
+                            >
+                              Confirm
+                            </Button>
+                            <Button
+                              variant="outlined"
+                              size="small"
+                              color="secondary"
+                              onClick={() => {
+                                if (
+                                  window.confirm(
+                                    "Are you sure you want to reject this order?"
+                                  )
+                                )
+                                  reject(order.booking_id);
+                              }}
+                            >
+                              Reject
+                            </Button>
                           </TableCell>
                         </TableRow>
                       ))}
